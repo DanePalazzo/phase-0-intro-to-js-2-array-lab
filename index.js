5734336cd1728fd0fa26f214cfbbdea824dbebff
Dane Palazzo <98612023+DanePalazzo@users.noreply.github.com>
2023-08-28 11:15:06 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat() {
+    return [...cats, "Broom"]
+}
+
+function prependCat() {
+    return ["Arnold", ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0,cats.length-1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
+
+/*
+function removeLastCat() {
+    let newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+
+function removeFirstCat() {
+    let newCats = [...cats]
+    newCats.shift()
+    return newCats
+}
+*/
